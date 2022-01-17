37f8b3c910f3793a7debdff3519a887af1dd6f78
FaithfulBreeze <faithfulbreeze@outlook.com>
2022-01-17 14:11:05 -0300

    melhoria no codigo js

--- MeusProjetos/Mini-Projeto02-MeuHub/script.js
+++ MeusProjetos/Mini-Projeto02-MeuHub/script.js
@@ -1,11 +1,5 @@
-const discord = document.querySelector('#discord')
-const redes = document.querySelector('#redesS')
-discord.addEventListener('click', discordClick)
-redes.addEventListener('click', redesIrTopo)
-function discordClick(){
+document.querySelector('#discord').addEventListener('click', ()=>{
     navigator.clipboard.writeText("FaithfulBreeze#7782")
     setTimeout(() => {alert('Copiado para a area de transferência!')}, 100);
-}
-function redesIrTopo(){
-    window.scrollTo(0, 0)
-}
+})
+document.querySelector('#redesS').addEventListener('click', ()=>{window.scrollTo(0, 0)})
