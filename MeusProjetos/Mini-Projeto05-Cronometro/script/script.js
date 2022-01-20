2fa60a0517942efd35ef68dc9bde53dc7874d3e2
FaithfulBreeze <faithfulbreeze@outlook.com>
2022-01-20 20:34:05 -0300

    fixed bug

--- MeusProjetos/Mini-Projeto05-Cronometro/script/script.js
+++ MeusProjetos/Mini-Projeto05-Cronometro/script/script.js
@@ -7,6 +7,7 @@ document.querySelector('#reset').addEventListener('click', resetContagem)
 const outmin = document.querySelector('#minutos')
 const outsec = document.querySelector('#segundos')
 const outmsec = document.querySelector('#milisegundos')
+let running = false
 
 var min = 00
 var sec = 00
@@ -14,39 +15,43 @@ var msec = 00
 var tempo = undefined
 
 function startContagem(){
-    tempo = setInterval(function(){
-        if(outmsec.innerHTML < 9){
-            outmsec.innerHTML = `0${msec}`
-        }else{
-            outmsec.innerHTML = `${msec}`
-        }
-
-        if(outsec.innerHTML <= 9){
-            outsec.innerHTML = `0${sec}`
-        }else{
-            outsec.innerHTML = `${sec}`
-        }
-
-        if(outmin.innerHTML <= 9){
-            outmin.innerHTML = `0${min}`
-        }else{
-            outmin.innerHTML = `${min}`
-        }
-
-        if(msec > 99){
-            outmsec.innerHTML = "00"
-            msec = 00
-            sec++
-        }else if(sec > 59){
-            outsec.innerHTML = "00"
-            sec = 00
-            min++
-        }
-        msec++
-    }, 10)
+    if(running == false){
+        running = true
+        tempo = setInterval(function(){
+            if(outmsec.innerHTML < 9){
+                outmsec.innerHTML = `0${msec}`
+            }else{
+                outmsec.innerHTML = `${msec}`
+            }
+    
+            if(outsec.innerHTML <= 9){
+                outsec.innerHTML = `0${sec}`
+            }else{
+                outsec.innerHTML = `${sec}`
+            }
+    
+            if(outmin.innerHTML <= 9){
+                outmin.innerHTML = `0${min}`
+            }else{
+                outmin.innerHTML = `${min}`
+            }
+    
+            if(msec > 99){
+                outmsec.innerHTML = "00"
+                msec = 00
+                sec++
+            }else if(sec > 59){
+                outsec.innerHTML = "00"
+                sec = 00
+                min++
+            }
+            msec++
+        }, 10)
+    }
 }
 
 function stopContagem(){
+    running = false
     clearInterval(tempo)
 }
 
