f07c981d5ea03e0881a0f7436d63585bf8df1722
FaithfulBreeze <faithfulbreeze@outlook.com>
2022-01-25 22:26:06 -0300

    fixing script

--- MeusProjetos/Mini-Projeto03-x²Calc/script.js
+++ MeusProjetos/Mini-Projeto03-x²Calc/script.js
@@ -3,22 +3,20 @@ document.querySelector('#calcular').addEventListener('click', (a = 0, b = 0, c =
     const coef1 = document.querySelector('#coef1')
     const coef2 = document.querySelector('#coef2')
     const coef3 = document.querySelector('#coef3') 
-    const res = []
 
     a = coef1.valueAsNumber
     b = coef2.valueAsNumber
     c = coef3.valueAsNumber
 
-    const DELTA = b**2-4*a*c
+    const DELTA = (b**2)-4*a*c
     
     if(DELTA < 0){
 
         alert('Calculo sem continuação, Delta é menor que 0')
 
     }else{
-        const x1 = (-b+Math.sqrt(DELTA))/2*a
-        const x2 = (-b-Math.sqrt(DELTA))/2*a
-        res.push(x1, x2)
-        alert(res)
+        const x1 = (-b+Math.sqrt(DELTA))/(2*a)
+        const x2 = (-b-Math.sqrt(DELTA))/(2*a)
+        alert(`X1 = ${x1}\nX2 = ${x2}\nΔ = ${DELTA}`)
     }
 })
